6190f6740f4740c11bb0482b2d39da7351bd68d6
deion.mcfarlane@gmail.com <deion.mcfarlane@gmail.com>
2025-12-29 17:57:03 +0000

    Created 5060.jpg, 5070.jpg, 7600.jpg, 7600gpu.jpg and 12 more; Edited PC_Parts and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -5,8 +5,9 @@ class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-  self.content_panel.add_component(Desktops())
+    self.content_panel.add_component(Checkout())
     # Any code you write here will run before the form opens.
+
 
   @handle("view_desktops", "click")
   def view_desktops_click(self, **event_args):

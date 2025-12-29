7e837c98ee9e21412780e96d3bbab6d212a21b0f
deion.mcfarlane@gmail.com <deion.mcfarlane@gmail.com>
2025-12-29 18:53:06 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -5,11 +5,11 @@ class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(Checkout())
     # Any code you write here will run before the form opens.
 
-
-  @handle("view_desktops", "click")
-  def view_desktops_click(self, **event_args):
+  @handle("button_3", "click")
+  def button_3_click(self, **event_args):
     """This method is called when the button is clicked"""
+    c = confirm("Add to cart?")
     pass
+alert("Welcome to CloudBound PCs")
